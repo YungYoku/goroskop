91dfbdbc9d1a1c05e70f65a9a5d859d974f7b6bc
Yoku <yungyoku@yandex.ru>
2024-12-25 16:28:41 +0300

    Added fetch for category id

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -6,16 +6,34 @@
 
 		<div class="category-info">
 			<div class="category-info__title">
-				Название
+				{{ data.title }}
 			</div>
 
 			<div class="category-info__description">
-				Описание
+				{{ data.description }}
 			</div>
 		</div>
 	</Page>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
 import Page from '@/components/page.vue'
+
+const data = ref({
+	title: 'Название',
+	description: 'Описание'
+})
+
+const loadData = async () => {
+	// Здесь меняет api роут на свой
+	await fetch('https://jsonplaceholder.typicode.com/posts/1')
+		.then(res => res.json())
+		.then(res => {
+			data.value.title = res.title
+			data.value.description = res.body
+		})
+}
+loadData()
 </script>
